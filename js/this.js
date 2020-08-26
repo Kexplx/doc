e4256c16a526ad70103aa1bf6738b391b42a5090
Oscar Rosner <oscar.rosner@web.de>
2020-08-26 13:33:05 +0200

    .

--- js/this.js
+++ js/this.js
@@ -7,9 +7,15 @@ function use() {
   console.log(this.a + ' is ' + this.b + ' years old');
 }
 
-use(); // undefined is undefined years old
+use(); // error: this is undefined
+
+// -----------------------------------------------------------
 
  // Execution context is provided to the function
 use.call(executionContext); // Oscar is 23 years old
 
 // use.call(executionContext, argsIfItHadAny);
+
+const bound = use.bind(executionContext);
+
+bound(); // Oscar is 23 years old
