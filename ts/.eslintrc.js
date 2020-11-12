2f3b1acef7b78b4c39e5a04e6e40a19a16531341
Oscar Rosner <oscar.rosner@web.de>
2020-11-12 09:42:24 +0100

    .

---
+++ ts/.eslintrc.js
@@ -0,0 +1,12 @@
+// npm i --save-dev eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
