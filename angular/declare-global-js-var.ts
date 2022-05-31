083eb343e7cbc6a78c3e61d6c9404b95a721605a
Oscar Rosner <oscar.rosner@web.de>
2022-05-31 15:02:26 +0200

    .

--- angular/declare-global-js-var.ts
+++ angular/declare-global-js-var.ts
@@ -6,3 +6,23 @@ declare const myGlobalVar: string;
 
 // Now the variable can be used anywhere in the app
 // just like other global variables like: console, window, ...
+
+
+
+
+// Extent "window" global with a new property
+// electron
+
+
+// We extend the global window object's type
+// with the API exposed to us by the preloader.
+
+export interface ElectronApi {
+  loadPreferences: () => Promise<void>;
+}
+
+declare global {
+  interface Window {
+    electronApi: ElectronApi;
+  }
+}
